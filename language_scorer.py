75669cd1a718cf2a24da3ca46b3083539083ac3c
Matt Dewey <mattfromseattle@me.com>
2018-05-03 21:15:43 -0700

    Updated file
    Updated to include function call.

--- language_scorer.py
+++ language_scorer.py
@@ -16,13 +16,12 @@ def lang_scorer(fname1, fname2):
 
     split_text = text.split()
 
-for t in split_text:
-
-    score1 = []
+    for t in split_text:
+      score1 = []
 
     for w in words:
         if (split_text.count(w)) > 0:
-        score1.append((w, split_text.count(w)))
+          score1.append((w, split_text.count(w)))
         score2 = 0
 
     for words,score in score1:
@@ -32,5 +31,7 @@ for t in split_text:
 
         final_score = sum(score for words, score in score1) / len(split_text)
         print(final_score)
-            score1.append(final_score)
+        score1.append(final_score)
         print(score1)
+
+lang_scorer('text.txt', 'words.txt')
