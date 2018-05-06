74cac94141bde9d26c8a6c7b17c823af26bd0022
Matt Dewey <mattfromseattle@me.com>
2018-05-05 21:15:16 -0700

    Updated functions
    Updates to process files correctly.

--- language_scorer.py
+++ language_scorer.py
@@ -1,13 +1,13 @@
 def lang_scorer(fname1, fname2):
-    with open(fname1, 'r') as fhandle: # Opens the first file as read only
-        words= fname1.read() # Reads the opened file
+    with open(fname1, 'r') as fhandle:  # Opens the first file as read only
+        data = fhandle.read()  # Reads the opened file
 
-    words = words.split("\n")
+    data = data.split("\n")
 
     with open(fname2, 'r') as fhandle:
-        text = fname2.read()
+        data = fhandle.read()
 
-    text = text.upper()
+    data = data.upper()
 
     punc = "!,.;:'\"?-`"
 
@@ -17,15 +17,15 @@ def lang_scorer(fname1, fname2):
     split_text = text.split()
 
     for t in split_text:
-      score1 = []
+        score1 = []
 
     for w in words:
         if (split_text.count(w)) > 0:
-          score1.append((w, split_text.count(w)))
+            score1.append((w, split_text.count(w)))
         score2 = 0
 
-    for words,score in score1:
-        print(words,score)
+    for words, score in score1:
+        print(words, score)
         score2 += score
         print(score2)
 
